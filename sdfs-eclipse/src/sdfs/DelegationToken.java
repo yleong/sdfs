7276ec80e936ee7e20a517c431d3ada4b72c42c4
Yew Long <yewlong42@gmail.com>
2013-04-27 20:45:05 -0400

    added the delegation token file

---
+++ sdfs-eclipse/src/sdfs/DelegationToken.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+package sdfs;
+
+public class DelegationToken implements java.io.Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8470093355687010538L;
+	private String fileName;
+	private String recipientName;
+	private String rights;
+	private int numdays; //number of seconds before expiry
+	private boolean propagate;
+
+	DelegationToken(){} //empty constructor
+	DelegationToken(String recipientName, String rights, int numdays, boolean propagate){
+	    this.recipientName = recipientName;
+	    this.rights = rights;
+	    this.numdays = numdays;
+	    this.propagate = propagate;	    
+	}
+
+	
+}
